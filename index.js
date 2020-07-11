6fa91d07c04f2e388bf71c8632397779346794ae
Kristin Barr <kristinbarr@gmail.com>
2020-07-11 14:54:19 -0400

    luis-guided

--- index.js
+++ index.js
@@ -1,43 +1,61 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const bcrypt = require('bcryptjs');
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const bcrypt = require('bcryptjs')
 
-const db = require('./database/dbConfig.js');
-const Users = require('./users/users-model.js');
+const db = require('./database/dbConfig.js')
+const Users = require('./users/users-model.js')
 
-const server = express();
+const server = express()
 
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
 
 server.get('/', (req, res) => {
-  res.send("It's alive!");
-});
+  res.send("It's alive!")
+})
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+  let { username, password } = req.body
+  const hash = bcrypt.hashSync(password, 10)
 
   // validate the user
-console.log('user:', user)
+  console.log('user:', user)
   // hash the password
   const hash = bcrypt.hashSync(user.password, 8);
 
   // we override the password with the hash
   user.password = hash;
 
-  Users.add(user)
+  Users.add({ username, password: hash })
     .then(saved => {
       res.status(201).json(saved);
     })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+    .catch((error) => {
+      res.status(500).json(error)
+    })
+})
 
 server.post('/api/login', (req, res) => {
-  let { username, password } = req.body;
+  let { username, password } = req.body
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then((user) => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` })
+        } else {
+          res.status(401).json({ message: 'You cannot pass!!' })
+        }
+      })
+      .catch((error) => {
+        res.status(500).json(error)
+      })
+  } else {
+    res.status(400).json({ message: 'please provide credentials' })
+  }
 
   if (username && password) {
     Users.findBy({ username })
@@ -59,11 +77,20 @@ server.post('/api/login', (req, res) => {
 
 server.get('/api/users', protected, (req, res) => {
   Users.find()
-    .then(users => {
-      res.json(users);
+    .then((users) => {
+      res.json(users)
     })
-    .catch(err => res.send(err));
-});
+    .catch((err) => res.send(err))
+})
+
+server.get('/hash', (req, res) => {
+  const name = req.query.name
+
+  // hash the name
+  // .hashSync will block thread
+  const hash = bcrypt.hashSync(name, 15) // use bcryptjs to hash the name
+  res.send(`the hash for ${name} is ${hash}`)
+})
 
 server.get('/hash', (req, res) => {
   // read a password from the Authorization header
